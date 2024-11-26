33b77004da427e72674fe28df46eb2154d09a766
OscarMIP <oscar.mataro.epiaedu.cat>
2024-11-26 20:52:42 +0100

    Finished: Completed all exercices

--- loops-conditionals/loops/exercise_00.js
+++ loops-conditionals/loops/exercise_00.js
@@ -3,3 +3,18 @@
     
     2. Create a for loop that will print out all the odd numbers between 10 and 40.
 */
+
+
+//ex1
+for (let i = 10; i <= 40; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+//ex2
+for (let i = 10; i <= 40; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
